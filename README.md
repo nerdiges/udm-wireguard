b3e11d2a12760b14ccb46f61c9bae0549e816119
Stephan Klein <stephan@nerdig.es>
2024-01-17 14:58:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# udm-setup-wg
-Wiregurad auf der UDM Pro manuell einrichten
+# udm-wireguard
+Manuelle Wiregurad Konfiguration auf der UDM Pro.
+
+In der GUI der UDM Pro können mittlerweile wireguard VPNs eingerichtet weden. Allerdings sind hier eher nur Standardkonfigurationen möglich. Komplexere Site2Site Setups mit speziellen Paramtern konnte ich über di GUI nicht nutzen. Stattdessen können mit udm-wireguard die entsprechenden Interfaces direkt und ohne GUI eingerichtet werden.
+
+## Voraussetzungen
+Unifi Dream Machine Pro mit UnifiOS Version 3.x. Erfolgreich getestet mit UnifiOS 3.2.7.
+
+## Funktionsweise
+Das Script `udm-wireguard.sh` wird bei jedem Systemstart und anschließend alle 2 Minuten per systemd ausgeführt. 
+
+Die eingerichteten Wireguard-Interfaces werden wie LAN-Interfaces behandelt und entsprechend im Firewall-Regelwerk integriert. Die dabei erstellten Regeln werden bei Änderungen an der Netzwerkkonfiguration in der GUI regelmäßig wieder überschrieben. Damit die VPN-Verbindungen dauerhaft integriert werden, wird regelmäßig überprüft, ob die Firewall-Regeln noch passen. Neben dem systemd-Service wird daher auch ein systemd-Timer eingerichtet der das Script alle 2 Minuten neu startet und so die Regeln bei Bedarf wiederherstellt.
+
+## Hinweis
+**ACHTUNG:** Die Wireguard Interfaces werden vom Script wie normale LAN-Interfaces behandelt! In der Standardkonfiguration können mit dem VPN verbundene Geräte auf alle Systeme und Dienste uneingeschränkt zugreifen. Für eine angemessene Netzwerktrennung müssen daher in der GUI entsprechende Regeln eingefügt werden. Alternativ kann auch [udm-firewall](https://github.com/nerdiges/udm-firewall) für die NEtzwerktrennung genutzt werden.
+
+## Installation
+Nachdem eine Verbindung per SSH zur UDM/UDM Pro hergestellt wurde wird udm-wireguard folgendermaßen installiert:
+
+### 1. Download der Dateien 
+
+```
+mkdir -p /data/custom
+dpkg -l git || apt install git
+git clone https://github.com/nerdiges/udm-wireguard.git /data/custom/wireguard
+chmod +x /data/custom/wireguard/udm-wireguard.sh
+```
+
+### 2. Parameter im Script anpassen (optional)
+Im Script kann über eine Variablen das Verzeichnis hinterlegt werden, in dem die wireguard Config-Files abgelegt werden:
+
+```
+# Directory with wireguard config files. All *.conf files in
+# the directory will be considered as valid wireguard configs
+conf_dir="/data/custom/wireguard/"
+```
+
+Dieser PArameter muss in der Regel nicht angepasst weden.
+
+### 3. Einrichten der systemd-Services
+Ist auf der UDM-Pro auch das Script [udm-firewall](https://github.com/nerdiges/udm-firewall) installiert, kann dieser Schritt übersprungen werden, da das Script automatisch von [udm-firewall](https://github.com/nerdiges/udm-firewall) mit ausgeführt wird. Damit das funktioniert müssen sowohl das [udm-firewall](https://github.com/nerdiges/udm-firewall) als auch udm-wireguard, wie in den jeweiligen README.md beschrieben installiert wurden. 
+
+```
+# Install udm-wireguard.service und timer definition file in /etc/systemd/system via:
+ln -s /data/custom/wireguard/udm-wireguard.service /etc/systemd/system/udm-wireguard.service
+ln -s /data/custom/wireguard/udm-wireguard.timer /etc/systemd/system/udm-wireguard.timer
+
+# Reload systemd, enable and start the service and timer:
+systemctl daemon-reload
+systemctl enable udm-wireguard.service
+systemctl start udm-wireguard.service
+systemctl enable udm-wireguard.timer
+systemctl start udm-wireguard.timer
+
+# check status of service and timer
+systemctl status udm-wireguard.timer udm-wireguard.service
+```
+
+### 4. Wireguard Config-Files
+Damit wireguard Interfaces angelegt werden, müssen die entsprechenden wireguard Config-Files erstellt und im Verzeichnis `$conf_dir` (siehe oben Punkt 2) abgelegt werden.
+
+Der Dateiname 
+
+
+## Known Issues
+Der DNS-Parameter in wireguard Config-Files führt zu einer Fehlermeldung, da das Paket openresolv im UnifiOS per Default nicht installiert ist. 
---
+++ udm-wireguard.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+######################################################################################
+#
+# Description:
+# ------------
+#	Initialize Wireguard setup, start wireguard tunnel and setup firewall rules
+#   for VPN tunnels. As firewall rules may be reseted whenever ruleset is changed
+#   in GUI, this script should be executed regularly to ensure that VPN firewall 
+#   is permanently activated.
+#
+######################################################################################
+
+##############################################################################################
+#
+# Configuration
+#
+
+# directory with wireguard config files. All *.conf files in
+# the directory will be considered as valid wireguard configs
+conf_dir="/data/custom/wireguard/"
+
+#
+##############################################################################################
+
+
+##############################################################################################
+#
+# No further changes should be necessary beyond this line.
+#
+
+# set scriptname
+me=$(basename $0)
+
+
+for conf_file in ${conf_dir}/*.conf; do
+    wg_if=$(basename $conf_file .conf)
+    wg show $wg_if || wg-quick up $conf_file
+
+    # As ruleset is reset, when changes are made via GUI it can be assumed that script
+    # can be stopped when wireguard interfaces are not considered in fw rule set.
+    if ( iptables --list-rules | grep -e "-i $wg_if " &&  ip6tables --list-rules | grep -e "-i $wg_if " ); then 
+        logger "$me: Found wireguard rules for $wg_if. Nothing to do."
+    else
+
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # add rule for VPN interface to UBIOS_INPUT_USER_HOOK  
+        #
+        rule="-A UBIOS_INPUT_USER_HOOK -i _IF_ -j UBIOS_LAN_LOCAL_USER"
+        iptables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv4 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv4  ${rule/_IF/$wg_if}"
+        ip6tables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv6 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv6 ${rule/_IF/$wg_if}"
+
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # add rule for VPN interface to UBIOS_FORWARD_IN_USER
+        #
+        rule="-A UBIOS_FORWARD_IN_USER -i _IF_ -j UBIOS_LAN_IN_USER"
+        iptables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv4 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv4  ${rule/_IF/$wg_if}"
+        ip6tables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv6 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv6 ${rule/_IF/$wg_if}"
+
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # add rule for VPN interface to UBIOS_FORWARD_OUT_USER 
+        #
+        rule="-A UBIOS_FORWARD_OUT_USER -o _IF_ -j UBIOS_LAN_OUT_USER"
+        iptables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv4 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv4  ${rule/_IF/$wg_if}"
+        ip6tables ${rule/_IF_/$wg_if} &&
+            logger "$me: added IPv6 rule: ${rule/_IF/$wg_if}" ||
+            logger "$me: failed to add IPv6 ${rule/_IF/$wg_if}"
+
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # add allow related/established to UBIOS_LAN_IN_USER if necessary
+        #
+        rule="-A UBIOS_LAN_IN_USER -m conntrack --ctstate RELATED,ESTABLISHED.*-j RETURN"
+        iptables --list-rules | grep -e "$rule" &> /dev/null || 
+            iptables -I UBIOS_LAN_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+        ip6tables --list-rules | grep -e "$rule" &> /dev/null || 
+            ip6tables -I UBIOS_LAN_IN_USER 1 -m conntrack --ctstate RELATED,ESTABLISHED -j RETURN
+
+    fi
+done
